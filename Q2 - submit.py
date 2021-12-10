86bdff7c274d14b59ea53133c0ba02d99d004b60
Jackdeknipper <81079010+Jackdeknipper@users.noreply.github.com>
2021-12-10 14:12:41 +0100

    Add files via upload

--- Q2 - submit.py
+++ Q2 - submit.py
@@ -1,59 +1,64 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-import numpy as np 
-from scipy.stats import norm
-from math import log
+# In[180]:
+
 
 #assignment exercise 2a
-import csv
-file = open('AEX-INDEX_quote_chart.csv')
-#file = open('AEX-INDEX_Historical_price.csv')
-reader = csv.reader(file, quoting=csv.QUOTE_ALL, skipinitialspace=True)
-AEX = [] #not integer!. First of all a list 'AEX' is created with the AEX index values that we retrieved
-for line in reader:
-    AEX.append(line[1])
-#The header and the first value which is from a different date  
-#compared to the other values (all a minute apart from each other) are deleted from 'AEX'
-del AEX[0:2]
-#next the differences in AEX index for each step are stored in AEXdif
+from datetime import datetime
+#first we download the file from https://live.euronext.com/en/product/indices/NL0000000107-XAMS
+#and form the a list with the AEX values
+file = open('AEX-INDEX_1.csv', 'r') 
+a = file.readlines()
+del a[0:4] 
+AEX = []
+dates = [] #we also form a list with the corresponding dates of the AEX values 
+for line in a:
+    b = line.split(";")
+    AEX.append(float(b[1]))
+    dates.append(b[0])
+    
+#Next we form a list dt containing the difference in date in days between each two succesive AEX values
+date_format = "%m/%d/%Y"
+dt=[]
+for i in range(len(dates)-1):
+    a = datetime.strptime(dates[i], date_format)
+    b = datetime.strptime(dates[i+1], date_format)
+    dt.append((a-b).days)
+dt[42] = 1
+
+#Then we also create a list AEXdif with the difference between two succesive AEX values
 AEXdif = []
 for i in range(len(AEX)-1):
-    AEXdif.append(float(AEX[i+1])-float(AEX[i]))
-#dXt = Xt(mu*dt + sigma*dW), so (mu*dt + sigma*dW) = dXt/Xt
-variable = []
+    AEXdif.append(AEX[i+1]-AEX[i])
+    
+#Next we use that dXt = Xt(mu*dt + sigma*dW), so (mu*dt + sigma*dW) = dXt/Xt
+variable = [] #variable will contain the values (mu*dt + sigma*dW)
 for i in range(len(AEXdif)):
     variable.append(AEXdif[i]/float(AEX[i]))
-mean = sum(variable) #Since E[sigma*dW] = 0, mean=mu*dt=mu
-var = sum((x-mean)**2 for x in variable) / (len(variable)-1) #this is equal to sigma**4
-sigma = np.sqrt(var)
-mu = mean + var/2
-print(mu, sigma)
+newvariable = [] #We create newvariable which will take the different dt's into account
+for i in range(len(AEXdif)):
+    newvariable.append(AEXdif[i]/(float(AEX[i])*dt[i]))
+mu = sum(newvariable)/len(newvariable) #Since E[sigma*dW] = 0, mean=mu*dt. As we already took dt into account in the previous step we get mu
+mean = sum(variable)/len(variable)
+var = sum((x-mean)**2 for x in variable) / (len(variable)-1) #this is equal to sigma
+print(mu, var)
 
 
-# In[88]:
+# In[182]:
 
 
 #2b
-
+import numpy as np
 
 def black_scholes_call(S, K, r, sigma, T):
     d = (np.log(S / K) + (r - 0.5 * sigma ** 2) * T) / (sigma * np.sqrt(T))
     price = np.exp(-r * T) * norm.cdf(d)
     return price
-S = float(AEX[0])
-r = .01
+S = AEX[0]
+r = 0.01 
 total_price = np.exp(-r*5)*100 
 for T in range(1,6):
     total_price += black_scholes_call(S, 850, r, sigma, T)
 print(total_price)
-
-
-# In[89]:
-
-
-
-
-
-# In[ ]:
 
